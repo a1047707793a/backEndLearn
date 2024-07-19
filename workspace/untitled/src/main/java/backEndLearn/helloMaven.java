707b2ee4257ba10e2cdcd55e19016a327ecb404a
a1047707793a <1047707793@qq.com>
2024-07-19 12:03:35 +0800

    backEndLearn

---
+++ workspace/untitled/src/main/java/backEndLearn/helloMaven.java
@@ -0,0 +1,11 @@
+package backEndLearn;
+
+
+import com.sun.org.slf4j.internal.Logger;
+
+public class helloMaven {
+    public static void main(String[] args) {
+        Logger log;
+        System.out.println("Hello Maven!");
+    }
+}
---
+++ workspace/untitled/src/test/java/backEndLearn/backEndlearn/DemoTest.java
@@ -0,0 +1,20 @@
+package backEndLearn.backEndlearn;
+
+import org.junit.Test;
+
+
+public class DemoTest {
+    @Test
+    public void test1() {
+        System.out.println("Hello Maven!1");
+    }
+    @Test
+    public void test2() {
+        System.out.println("Hello Maven!1");
+    }
+    @Test
+    public void test3() {
+        System.out.println("Hello Maven!1");
+    }
+
+}

5cac68a4a83ca2900e728c1e884899d0fc24f8fe
a1047707793a <1047707793@qq.com>
2024-07-19 12:11:29 +0800

    backEndLearn

--- workspace/untitled/src/test/java/backEndLearn/backEndlearn/DemoTest.java
+++ workspace/untitled/src/test/java/backEndLearn/backEndlearn/DemoTest.java
@@ -16,5 +16,9 @@ public class DemoTest {
     public void test3() {
         System.out.println("Hello Maven!1");
     }
+    @Test
+    public void test4() {
+        System.out.println("Hello Maven!1");
+    }
 
 }
